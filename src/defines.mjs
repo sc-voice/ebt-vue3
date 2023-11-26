bb5b35abc5e709460fca73383107b634721d1e70
Karl Lew <karl@firepick.org>
2023-11-26 09:23:46 -0800

    v1.379.0 += defines

--- src/defines.mjs
+++ src/defines.mjs
@@ -1,7 +1,7 @@
-export const DEBUG_FOCUS = true;
-export const DEBUG_SCROLL = true;
+export const DEBUG_FOCUS = false;
+export const DEBUG_SCROLL = false;
 export const DEBUG_PATH = false;
 export const DEBUG_LOAD = false;
-export const DEBUG_WIKI = true;
-export const DEBUG_SEARCH = true;
+export const DEBUG_WIKI = false;
+export const DEBUG_SEARCH = false;
 
