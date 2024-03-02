e7d8d908df39613fd12993359d6c1cd208a03395
sabbamitta <43345786+sabbamitta@users.noreply.github.com>
2024-03-02 16:00:25 +0100

    Sammlung wiedergeben

--- src/i18n/de.mjs
+++ src/i18n/de.mjs
@@ -264,7 +264,7 @@ export default {
     "playEnd": "Sutta wiedergeben …",
     "playEndRepeat": "Sutta wiedergeben und wiederholen",
     "playEndStop": "Wiedergabe bis Ende des Sutta",
-    "playEndTipitaka": "Tipitaka wiedergeben",
+    "playEndTipitaka": "Sammlung wiedergeben",
     "playSection": "Abschnitt A_SECTION wiedergeben",
     "point": "Punkt",
     "processing": "Verarbeiten...",
