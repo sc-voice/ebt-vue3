372392cbfa2b8833e73d19c396ddf4f968fb2b66
sabbamitta <43345786+sabbamitta@users.noreply.github.com>
2024-04-22 08:43:24 +0200

    copyright

--- src/i18n/de.mjs
+++ src/i18n/de.mjs
@@ -244,7 +244,7 @@ export default {
     "languageCode": "Deutsch / DE",
     "languages": "Sprachen",
     "latestContent": "Neuester Text für Sutta/Vinaya",
-    "license": "License",
+    "license": "Copyright",
     "loadingAudio": "Audio laden...",
     "loadingSutta": "wird geladen...",
     "logLevel": "Konsolen-Info",
